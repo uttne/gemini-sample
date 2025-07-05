df5edac3a6a4dda61582046028984aa17ab20b9c
uttne <uttne.mkr@gmail.com>
2025-07-05 12:19:40 +0900

    テストの追加

---
+++ tests/App.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import App from '../src/App';
+import { describe, it, expect, vi } from 'vitest';
+
+vi.mock('react-i18next', () => ({
+  useTranslation: () => {
+    return {
+      t: (str: string) => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
+  I18nextProvider: ({ children }: { children: React.ReactNode }) => children,
+}));
+
+describe('App', () => {
+  it('renders App component', () => {
+    render(<App />);
+    expect(screen.getByText('appTitle')).toBeInTheDocument();
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './tests/setupTests.ts',
+  reporters: ['default', 'junit'],
+    outputFile: {
+      junit: `./test-results/junit-${new Date().toISOString().replace(/:/g, '-')}.xml`,
+    },
+  },
 });
